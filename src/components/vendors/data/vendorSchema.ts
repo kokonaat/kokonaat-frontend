555c67d7c6297bdb4ea8708047e50835a720b652
dassajib <sajibdas.sd@gmail.com>
2025-10-05 17:54:35 +0600

    row action issue fix when click

---
+++ src/components/vendors/data/vendorSchema.ts
@@ -0,0 +1,16 @@
+import { z } from "zod";
+
+export const vendorSchema = z.object({
+  id: z.string(),
+  name: z.string(),
+  email: z.string().nullable(),
+  phone: z.string(),
+  address: z.string(),
+  city: z.string().nullable(),
+  country: z.string().nullable(),
+  isB2B: z.boolean().default(false),
+  contactPerson: z.string().nullable(),
+  contactPersonPhone: z.string().nullable(),
+});
+
+export type Vendor = z.infer<typeof vendorSchema>

56c2f4f8ad0658920356e88c406797adffc94f45
avee <aveechakra@gmail.com>
2025-12-23 02:37:04 +0600

    Update yarn.lock and enhance report column formatting,  Updated dependencies in yarn.lock, including new versions for @emnapi/core, @emnapi/runtime, and various esbuild packages.Improved formatting in ReportColumns.tsx to handle undefined values gracefully, ensuring 'N/A' is displayed when inventory name, quantity, or unit of measurement is not available.

--- src/components/report/ReportColumns.tsx
+++ src/components/report/ReportColumns.tsx
@@ -86,7 +86,7 @@ export const TransactionReportColumns: ColumnDef<TransactionReportItem>[] = [
       const fullText = details
         .map(
           (d) =>
-            `${d.inventory?.name} (${d.quantity} ${d.unitOfMeasurement.name})`
+            `${d.inventory?.name ?? 'N/A'} (${d.quantity ?? 'N/A'} ${d.unitOfMeasurement?.name ?? 'N/A'})`
         )
         .join(", ")
 
